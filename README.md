a611d73c700bded974ea9c0a263bc1f11d798424
Nguyen Tran <merlinvn@gmail.com>
2021-08-10 21:56:39 -0400

    new system

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # plover_vietneo
+
+
+# TODO
++ Visiualization
++ Regenerate dictionary
++ Adapt https://github.com/JoshuaGrams/steno-jig to Vietnamese
+
+# References
+Inspiration and dictionary
+https://github.com/aerickt/plover_viet
+
+Vietnamese corpus
+https://github.com/undertheseanlp/NLP-Vietnamese-progress/blob/master/tasks/resources.md
---
+++ plover_vietneo/system.py
@@ -0,0 +1,101 @@
+# vim: set fileencoding=utf-8 :
+
+# #CTNRGLHUOA*IEZWNCTMSFRX
+KEYS = (
+    '#',
+    'C-', 'T-', 'N-', 'R-', 'G-', 'L-', 'H-', 'U-',
+    'O-', 'A-',
+    '*',
+    '-I', '-E',
+    '-Z', '-W', '-N', '-C', '-T', '-M', '-S', '-F', '-R', '-X',
+)
+
+IMPLICIT_HYPHEN_KEYS = ('U-', 'O-', 'A-', '-I', '-E', '*', '-W')
+#IMPLICIT_HYPHEN_KEYS = KEYS
+
+SUFFIX_KEYS = ()
+
+NUMBER_KEY = '#'
+
+NUMBERS = {
+    'C-': '1-',
+    'N-': '2-',
+    'G-': '3-',
+    'H-': '4-',
+    'O-': '5-',
+    'A-': '0-',
+    '-Z': '-6',
+    '-N': '-7',
+    '-T': '-8',
+    '-S': '-9',
+}
+
+UNDO_STROKE_STENO = '*'
+
+ORTHOGRAPHY_RULES = []
+
+ORTHOGRAPHY_RULES_ALIASES = {}
+
+ORTHOGRAPHY_WORDLIST = None
+
+KEYMAPS = {
+    'Gemini PR': {
+        '#'         : ('#1', '#2', '#3', '#4', '#5', '#6', '#7', '#8', '#9', '#A', '#B', '#C'),
+        'C-'        : 'S1-',
+        'T-'        : 'S2-',
+        'N-'        : 'T-',
+        'R-'        : 'K-',
+        'G-'        : 'P-',
+        'L-'        : 'W-',
+        'H-'        : 'H-',
+        'U-'        : 'R-',
+        'O-'        : 'A-',
+        'A-'        : 'O-',
+        '*'         : ('*1', '*2', '*3', '*4'),
+        '-I'        : '-E',
+        '-E'        : '-U',
+        '-Z'        : '-F',
+        '-W'        : '-R',
+        '-N'        : '-P',
+        '-C'        : '-B',
+        '-T'        : '-L',
+        '-M'        : '-G',
+        '-S'        : '-T',
+        '-F'        : '-S',
+        '-R'        : '-D',
+        '-X'        : '-Z',
+        'no-op'     : ('Fn', 'pwr', 'res1', 'res2'),
+    },
+    'Keyboard': {
+        '#'         : ('1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '='),
+        'C-'        : 'q',
+        'T-'        : 'a',
+        'N-'        : 'w',
+        'R-'        : 's',
+        'G-'        : 'e',
+        'L-'        : 'd',
+        'H-'        : 'r',
+        'U-'        : 'f',
+        'O-'        : 'c',
+        'A-'        : 'v',
+        '*'         : ('t', 'g', 'y', 'h'),
+        '-I'        : 'n',
+        '-E'        : 'm',
+        '-Z'        : 'u',
+        '-W'        : 'j',
+        '-N'        : 'i',
+        '-C'        : 'k',
+        '-T'        : 'o',
+        '-M'        : 'l',
+        '-S'        : 'p',
+        '-F'        : ';',
+        '-R'        : '[',
+        '-X'        : '\'',
+        'arpeggiate': 'space',
+        # Suppress adjacent keys to prevent miss-strokes.
+        'no-op'     : ('z', 'x', 'b', ',', '.', '/', ']', '\\'),
+    },
+}
+
+DICTIONARIES_ROOT = 'asset:plover_vietneo:dictionaries'
+DEFAULT_DICTIONARIES = ('vietneo_user.json', 'vietneo_commands.json', 'vietneo_main.json')
